cbf4a18044cb1f0e4f02baa120a3c987bd0f89c8
Your Name <you@example.com>
2021-12-22 14:12:52 +0800

    Student

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    int id;
+    String name;
+    String  age;
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
